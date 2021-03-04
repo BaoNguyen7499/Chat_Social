d0c0fba485f6714a02d38bb4459ad60d612bedf8
BaoNguyen7499 <67630732+BaoNguyen7499@users.noreply.github.com>
2021-03-04 21:28:20 +0700

    [1.0.0] finish register

--- lang/vi.js
+++ lang/vi.js
@@ -5,3 +5,15 @@ export const transValidation = {
     password_confirmation_incorrect: "Nhập lại mật khẩu chưa chính xác.",
 
 };
+
+export const transErrors = {
+    account_in_use: "Email này đã được sử dụng.",
+    account_removed: "Tài khoản này đã bị gỡ khỏi hệ thống, có vấn đề xin vui lòng liên hệ với chung tôi.",
+    account_not_active: "Email đã được đăng ký nhưng chưa active tài khoản, kiểm tra email hoặc vui lòng liên hệ với hệ thống hỗ trợ của chúng tôi.",
+};
+
+export const transSuccess = {
+    userCreated: (userEmail) => {
+        return `Tài khoản <strong>${userEmail}</strong> đã được tạo, vui lòng kiểm tra email của bạn để active tài khoản trước khi đăng nhập. Xin cảm ơn!`;
+    }
+};
