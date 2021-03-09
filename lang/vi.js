8e118fbf13ebcba3cca81e32dbb8e5ae9fb35da0
BaoNguyen7499 <67630732+BaoNguyen7499@users.noreply.github.com>
2021-03-09 15:00:25 +0700

    [1.0.0] Send email confirm accout.

--- lang/vi.js
+++ lang/vi.js
@@ -10,10 +10,25 @@ export const transErrors = {
     account_in_use: "Email này đã được sử dụng.",
     account_removed: "Tài khoản này đã bị gỡ khỏi hệ thống, có vấn đề xin vui lòng liên hệ với chung tôi.",
     account_not_active: "Email đã được đăng ký nhưng chưa active tài khoản, kiểm tra email hoặc vui lòng liên hệ với hệ thống hỗ trợ của chúng tôi.",
+    token_undefined: "Token không tồn tại!"
 };
 
 export const transSuccess = {
     userCreated: (userEmail) => {
         return `Tài khoản <strong>${userEmail}</strong> đã được tạo, vui lòng kiểm tra email của bạn để active tài khoản trước khi đăng nhập. Xin cảm ơn!`;
-    }
+    },
+    account_active: "Kích hoạt tài khoản thành công, bạn đã có thể đăng nhập vào ứng dụng"
 };
+
+export const transMail = {
+    subject: "Chat Social: Xác nhận kích hoạt tài khoản",
+    template: (linkVerify) => {
+        return `
+        <h2>Bạn nhận được email này vì đã đăng ký tài khoản trên ứng dụng Chat Social.</h2>
+        <h3>Vui lòng click vào liên kết bên dưới để xác nhận kích hoạt tài khoản.</h3>
+        <h3><a href="${linkVerify}" target="blank">${linkVerify}</a></h3>
+        <h4>Nếu tin rằng email này là nhầm lẫn hãy bỏ qua nó. Trân trọng.</h4>
+        `;
+    },
+    send_failed: "Có lỗi trong quá trình gửi email, vui lòng liên hệ với bộ phận hỗ trợ bên chung tôi"
+}
